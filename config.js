4b5832c2659ad517d94b792d62754c1606e75e11
Stanislav Dryomov <stasundr@air.public.wireless.med.harvard.edu>
2015-07-23 14:35:49 -0400

    Introducing f3.js

---
+++ config.js
@@ -0,0 +1,12 @@
+path = require('path');
+
+module.exports = {
+    mail: 'stasundr@gmail.com',
+    history: '/home/sd245/history/',
+
+    geno: '/groups/reich/DAVID/V8.2/V8.2.2/1240k_HO.geno',
+    ind:  '/groups/reich/DAVID/V8.2/V8.2.2/1240k_HO.ind',
+    snp:  '/groups/reich/DAVID/V8.2/V8.2.2/1240k_HO.snp',
+
+    f3: '/home/np29/bin/qp3Pop'
+};
---
+++ f3.js
@@ -0,0 +1,55 @@
+var config = require('./config');
+
+// dependecies
+var mkdirp = require('mkdirp');
+var Set = require("collections/set");
+var fs = require('fs');
+var spawn = require('child_process').spawn;
+var crypto = require('crypto');
+var path = require('path');
+
+// prepare folder
+var token = 'f3_' + crypto.createHash('md5').update(Date.now() + Math.random() + '').digest('hex');
+var folder = config.history + token;
+var encoding = { encoding: 'ascii' };
+
+// prepare files
+var f3sh = '#!/usr/bin/env bash\n\nbsub -q short -W 720 -u ' + config.mail + ' < f3.bsub';
+
+var f3bsub = config.f3 + ' -p ' + path.join(folder, 'f3.par') + ' > ' + path.join(folder, 'f3.out');
+
+var f3par = 'genotypename: ' + config.geno
+    + '\nindivname:    ' + config.ind
+    + '\nsnpname:      ' + config.snp
+    + '\npopfilename:  ' + path.join(folder, 'f3.pfn');
+
+var pref3pfn = new Set (
+    require('fs')
+        .readFileSync(config.ind, { encoding: 'ascii' })
+        .split('\n')
+        .filter(function(s){ return s !== '' })
+        .map(function(s) { return (s.match(/[A-Za-z.-_]+$/) + '') })
+).toArray()
+    .filter(function(s) {
+        return !(s.match(/FAIL$/)) && !(s.match(/^Ignore/))
+    });
+
+var f3pfn = [];
+pref3pfn.forEach(function(s) {
+    if (process.argv[2] == 'X') f3pfn.push(s + ' ' + process.argv[3] + ' ' + process.argv[4]);
+    if (process.argv[3] == 'X') f3pfn.push(process.argv[2] + ' ' + s + ' ' + process.argv[4]);
+    if (process.argv[4] == 'X') f3pfn.push(process.argv[2] + ' ' + process.argv[3] + ' ' + s);
+});
+f3pfn = f3pfn.join('\n');
+
+// mkdir, save files, run
+mkdirp(folder, function(error) {
+    if (error) throw error;
+
+    fs.writeFileSync(path.join(folder, 'f3.sh'), f3sh, encoding);
+    fs.writeFileSync(path.join(folder, 'f3.par'), f3par, encoding);
+    fs.writeFileSync(path.join(folder, 'f3.pfn'), f3pfn, encoding);
+    fs.writeFileSync(path.join(folder, 'f3.bsub'), f3bsub, encoding);
+
+    var sh = spawn('sh', ['f3.sh']);
+});
